55715dbfbce1b3b711e986a7fde5c457457c8990
MSS <sarmadsaleem.333@gmail.com>
2025-02-04 01:29:44 +0500

    Structured the whole project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Muhammad Sarmad Saleem(MSS)",
+    author_email="sarmadsaleem.333@gmail.com",
+    packages=find_packages()
+)
