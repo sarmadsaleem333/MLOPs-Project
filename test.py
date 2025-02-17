bee0e8026cf0ac81ce9e00e78ed828c3184950d9
MSS <sarmadsaleem.333@gmail.com>
2025-02-18 01:15:29 +0500

    Added model pusher to aws

--- test.py
+++ test.py
@@ -1,27 +1,4 @@
-from pymongo import MongoClient
+import os
 
-# Your MongoDB Atlas connection string
-CONNECTION_URL = "mongodb+srv://sarmadsaleem333:4FDygQmrMY9ZZkra@cluster0.yfdm9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-DATABASE_NAME = "Proj1"
-COLLECTION_NAME = "Proj1-Data"
-
-
-# Connect to MongoDB Atlas
-import pandas as pd
-import pymongo
-
-df = pd.read_csv('C:/Users/sarma/Desktop/MLOPs-Project/notebook/data.csv')
-
-print(df.head())
-data = df.to_dict(orient='records')
-
-client = MongoClient(CONNECTION_URL)
-
-data_base = client[DATABASE_NAME]
-collection = data_base[COLLECTION_NAME]
-print("Connected successfully!!!")
-
-rec = collection.insert_many(data)
-
-print("Data inserted with record ids",)
+print("AWS_ACCESS_KEY_ID:", os.getenv("AWS_ACCESS_KEY_ID"))
+print("AWS_SECRET_ACCESS_KEY:", os.getenv("AWS_SECRET_ACCESS_KEY"))
